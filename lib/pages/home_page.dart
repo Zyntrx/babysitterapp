3fb48cc0a1635c8cf392d7add0a5619acd10fe38
rem <94621115+Zyntrx@users.noreply.github.com>
2024-10-05 09:29:09 +0800

    modify homepage and add main menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,126 @@
+import 'package:babysitterapp/pages/notification_page.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+const _navBarItems = [
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home_outlined),
+    activeIcon: Icon(Icons.home_rounded),
+    label: 'Home',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.person_outline_rounded),
+    activeIcon: Icon(Icons.person_rounded),
+    label: 'Profile',
+  ),
+];
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  Widget _getSelectedPage() {
+    switch (_selectedIndex) {
+      case 0:
+        return Column(
+          children: [
+            Image.asset(
+              'assets/images/app-logo.png',
+              fit: BoxFit.cover,
+            ),
+            const Text("Tiny Hands, Big Hearts"),
+            const SizedBox(height: 20),
+          ],
+        );
+      case 1:
+        return const Center(child: Text("Profile Page"));
+      default:
+        return const Center(child: Text("Home Page"));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final bool isSmallScreen = width < 600;
+    final bool isLargeScreen = width > 800;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Baby Sitter App"),
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/app-logo.png',
-            fit: BoxFit.cover,
-          )
+      drawer: Drawer(
+        // Add Drawer here
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 118, 8, 45),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications_active_sharp),
+              title: const Text('Notification'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NotificationPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: isSmallScreen
+          ? BottomNavigationBar(
+              items: _navBarItems,
+              currentIndex: _selectedIndex,
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            )
+          : null,
+      body: Row(
+        children: <Widget>[
+          if (!isSmallScreen)
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              extended: isLargeScreen,
+              destinations: _navBarItems
+                  .map((item) => NavigationRailDestination(
+                      icon: item.icon,
+                      selectedIcon: item.activeIcon,
+                      label: Text(
+                        item.label!,
+                      )))
+                  .toList(),
+            ),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: _getSelectedPage(),
+          ),
         ],
       ),
     );
